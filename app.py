65f7612da9c87d0e8074a0b599430251f8a18065
madienmex <madienmex94@gmail.com>
2024-01-14 13:07:28 -0600

    enabled CORS routines fixed __main__ running in debug with docker updated image generation end point

--- app.py
+++ app.py
@@ -5,10 +5,13 @@ from loopchat import run_conversation
 from loopimg import gen_image
 import os, openai
 
+#Variable init
 openai.api_key = os.getenv('OPENAI_API_KEY')
+#Global variable to store messages
+messages = []
 
 app = Flask(__name__)
-#CORS(app, origins=["http://127.0.0.1:5500"])
+CORS(app, resources={r"/*": {"origins": ["http://127.0.0.1:5500"]}}, supports_credentials=True)
 
 #sign up button
 @app.route('/signup', methods=['POST'])
@@ -43,7 +46,7 @@ def login():
 #Initialize ChatGPT
 @app.route('/initialize', methods=['POST'])
     # Header authorization attributes from CORS
-#@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def initialize():
     api_key = request.json.get('api_key')
     if api_key is not None:
@@ -54,7 +57,7 @@ def initialize():
 
 @app.route('/selfstart', methods=['POST'])
     # Header authorization attributes from CORS
-#@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def selfstart():
     api_key = os.getenv("OPENAI_API_KEY")
     if api_key is not None:
@@ -66,6 +69,7 @@ def selfstart():
 
 #chatbot
 @app.route('/chat', methods=['POST'])
+@cross_origin(origin='*', headers=['Content-Type', 'application/json'])
 def chat():
     if not config.get_api_key():
         return jsonify({"error": "API key not initialized"}), 401  # Unauthorized
@@ -100,4 +104,4 @@ def img():
 if __name__ == '__main__':
     if not os.path.exists('users.txt'):
         open('users.txt', 'w').close()
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000)
